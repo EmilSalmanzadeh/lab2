88f5a08352899de99af76de1db410370d52ccc31
EmilSalmanzadeh <35476735+EmilSalmanzadeh@users.noreply.github.com>
2021-01-14 11:25:49 -0800

    1/14 11:25 am

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -2,6 +2,14 @@ $(document).ready(function() {
     initializePage();
 });
 
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#7fff00");
+}
+
 function initializePage() {
     $('#testjs').click(function(e) {
             $('.jumbotron h1').text("Javascript is connected");
@@ -11,10 +19,4 @@ function initializePage() {
     $("a.thumbnail").click(projectClick);
 }
 
-function projectClick(e) { 
-    // prevent the page from reloading      
-    e.preventDefault();
-    // In an event handler, $(this) refers to      
-    // the object that triggered the event      
-    $(this).css("background-color", "#7fff00");
-}
+
